8b2b65efdb79c5729a8fc626b3390aedfc080663
Alexander <a.prams1@gmx.de>
2019-04-02 16:48:10 +0200

    config automatically creates plot folder

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 ROOT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 PLOT_FOLDER = os.path.join(ROOT_FOLDER, "plots")
+PLOT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), PLOT_FOLDER)
+os.makedirs(PLOT_FOLDER, exist_ok=True)
 
 EXPERIMENT_RESULTS_FOLDER = "experiment_results"
 EXPERIMENT_RESULTS_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), EXPERIMENT_RESULTS_FOLDER)
